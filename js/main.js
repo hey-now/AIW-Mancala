d6157cbd2684db2fab01988318895619bd45a67d
hey-now <rubinand22@gmail.com>
2022-10-07 17:28:29 -0700

    updating JS for help button

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 document.getElementById('help').addEventListener('click', handleInstruct);
 
 function handleInstruct(evt) {
-    document.getElementById('logo').style.visibility('hidden');
-    document.getElementById('logo2').style.visibility('hidden');
-    document.getElementById('instructions').style.visibility('visible');
+    evt.preventDefault();
+    const logoEl = document.getElementById('logo').style.visibility = 'hidden';
+    document.getElementById('logo2').style.visibility = 'hidden';
+    document.getElementById('instructions').style.visibility = 'visible';
+    console.log(logoEl);
 }

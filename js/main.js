f5c8711b0900c17c431506f9fb2bad3bcc675617
hey-now <rubinand22@gmail.com>
2022-10-10 13:04:45 -0700

    adding numbers to board

--- js/main.js
+++ js/main.js
@@ -1,16 +1,36 @@
 /*----- constants -----*/
+let randomStone[0] = "https://imgur.com/hBa9Y56.png";
+let randomStone[1] = "https://imgur.com/xoOWF8u.png";
+let randomStone[2] = "https://imgur.com/FbSPrUb.png";
+let randomStone[3] = "https://imgur.com/cm26GoN.png";
+let randomStone[4] = "https://imgur.com/zmHX8z2.png";
+let randomStone[5] = "https://imgur.com/XQcilwv.png";
+
 
 /*----- app's state (variables) -----*/
 
-let board, turn, winner;
+let board, turn, winner, stones;
 
 /*----- cached element references -----*/
+
+document.getElementById('avengersPlanets');
+document.getElementById('thanosPlanets');
+
 
 /*----- event listeners -----*/
 
 document.getElementById('help').addEventListener('click', handleInstruct);
 
 /*----- functions -----*/
+initialize();
+
+function initialize() {
+    render();
+}
+
+function render() {
+
+}
 
 function handleInstruct(evt) {
     evt.preventDefault();
@@ -25,3 +45,15 @@ function handleInstruct(evt) {
     }
 }
 
+function rndNum (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+    console.log(rndNum)
+}
+
+function pickRandomStones() {
+    let randNum = rndNum(1, 6);
+    const randomStone = new Array();
+    return document.getElementById("thanosPlanets").innerHTML = '<img src="'+randomStone[randNum]+'" />';
+}
